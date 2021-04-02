0dd70066af59022dd662c4f72b7dcbeecf2f5a7b
pengwei08 <pengwei08@baidu.com>
2021-04-02 15:08:36 +0800

    优化 Hello demo

---
+++ V8Engine.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Peng,Wei(BAMRD) on 2021/4/2.
+//
+
+
+#include "V8Engine.h"
+
+V8Engine::V8Engine() {
+    v8::V8::InitializeICU();
+    platform = v8::platform::NewDefaultPlatform();
+    v8::V8::InitializePlatform(platform.get());
+    v8::V8::Initialize();
+    this->create_params = v8::Isolate::CreateParams();
+    this->create_params.array_buffer_allocator =
+            v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    this->isolate = v8::Isolate::New(this->create_params);
+}
+
+V8Engine::~V8Engine() {
+    printf("~V8Engine\n");
+//    isolate->Dispose();
+//    v8::V8::Dispose();
+//    v8::V8::ShutdownPlatform();
+//    delete this->create_params.array_buffer_allocator;
+}
+
+bool V8Engine::runScript(const std::string &js, v8::Local<v8::Value>& result) const {
+    v8::Local<v8::Context> context = v8::Context::New(isolate);
+    v8::Context::Scope context_scope(context);
+    {
+        v8::Local<v8::String> source =
+                v8::String::NewFromUtf8(isolate, js.c_str()).ToLocalChecked();
+        v8::Local<v8::Script> script =
+                v8::Script::Compile(context, source).ToLocalChecked();
+        result = script->Run(context).ToLocalChecked();
+    }
+    return true;
+}
