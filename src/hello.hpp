ef1b913ab72f245fd8efc8b52162a94aa2691024
pengwei08 <pengwei08@baidu.com>
2021-04-06 17:10:50 +0800

    完善 v8pp 绑定 C++ 方法的示例

--- src/hello.hpp
+++ src/hello.hpp
@@ -29,17 +29,18 @@ extern void say_hello(char* argv[]) {
         v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
         v8pp::module myLib(isolate);
         myLib.set_const("PI", 3.1415);
-//        global->Set(isolate,"myLib", myLib.new_instance());
         // Create a new context.
         v8::Local<v8::Context> context = v8::Context::New(isolate, nullptr, global);
 
         // Enter the context for compiling and running the hello world script.
         v8::Context::Scope context_scope(context);
-
         {
+            v8::Maybe<bool> success = context->Global()->Set(context, v8::String::NewFromUtf8Literal(isolate, "myLib"),
+                                   myLib.new_instance());
+            printf("context->Global()->Set() success=%d\n", success.FromJust());
             // Create a string containing the JavaScript source code.
             v8::Local<v8::String> source =
-                    v8::String::NewFromUtf8Literal(isolate, "'#Hello' + ', World!'");
+                    v8::String::NewFromUtf8Literal(isolate, "myLib.PI + '#Hello' + ', World!'");
 
             // Compile the source code.
             v8::Local<v8::Script> script =
@@ -50,7 +51,7 @@ extern void say_hello(char* argv[]) {
 
             // Convert the result to an UTF8 string and print it.
             v8::String::Utf8Value utf8(isolate, result);
-            printf("%s\n", *utf8);
+            printf("result = %s\n", *utf8);
         }
         // Dispose the isolate and tear down V8.
         isolate->Dispose();
